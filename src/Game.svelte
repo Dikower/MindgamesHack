e001f4321a1aafb07bcd5b888e3f68c2380e6712
kuvshinovmark <kuvshinovmark2001@gmail.com>
2021-04-21 16:08:34 +0300

    KillingWhitesChips
    Реализация удаления белых фишек, если черные "атакуют", аналогичная реализация с удалением черных будет позже + будет запрет расстановки фишек

--- src/Game.svelte
+++ src/Game.svelte
@@ -6,11 +6,129 @@
   let sizeBoard = kollSqInLine * sizeSq;
   let massEl = [];
   let gameState = "black";
+
   function funcPaint(x, y) {
-    console.log(y, x);
-    console.log(massEl);
+    //функция очистки
+
+    function funcProv(x, y, color) {
+      console.log(x, y);
+      matrixForProv[y][x] = 1;
+      if (x + 1 < kollSqInLine) {
+        if (massEl[y][x + 1].state === "bisque") {
+          stateProv = false;
+        }
+        if (massEl[y][x + 1].state === color && matrixForProv[y][x + 1] === 0) {
+          funcProv(x + 1, y, color);
+        }
+      }
+      if (y + 1 < kollSqInLine) {
+        if (massEl[y + 1][x].state === "bisque") {
+          stateProv = false;
+        }
+        if (massEl[y + 1][x].state === color && matrixForProv[y + 1][x] === 0) {
+          funcProv(x, y + 1, color);
+        }
+      }
+      if (x - 1 >= 0) {
+        if (massEl[y][x - 1].state === "bisque") {
+          stateProv = false;
+        }
+        if (massEl[y][x - 1].state === color && matrixForProv[y][x - 1] === 0) {
+          funcProv(x - 1, y, color);
+        }
+      }
+      if (y - 1 >= 0) {
+        if (massEl[y - 1][x].state === "bisque") {
+          stateProv = false;
+        }
+        if (massEl[y - 1][x].state === color && matrixForProv[y - 1][x] === 0) {
+          funcProv(x, y - 1, color);
+        }
+      }
+    }
+    function funcPocras(x, y, color) {
+      massEl[y][x].state = "bisque";
+      if (x + 1 < kollSqInLine) {
+        if (massEl[y][x + 1].state === color) {
+          funcPocras(x + 1, y, color);
+        }
+      }
+      if (y + 1 < kollSqInLine) {
+        if (massEl[y + 1][x].state === color) {
+          funcPocras(x, y + 1, color);
+        }
+      }
+      if (x - 1 >= 0) {
+        if (massEl[y][x - 1].state === color) {
+          funcPocras(x - 1, y, color);
+        }
+      }
+      if (y - 1 >= 0) {
+        if (massEl[y - 1][x].state === color) {
+          funcPocras(x, y - 1, color);
+        }
+      }
+    }
+    // console.log("Нажали на ", y, x);
+    let matrixForProv = [];
+    let stateProv = true;
+
+    function funcPeresobratMtrix() {
+      matrixForProv = [];
+      for (let i = 0; i < kollSqInLine; i++) {
+        matrixForProv.push([]);
+        for (let q = 0; q < kollSqInLine; q++) {
+          matrixForProv[i].push(0);
+        }
+      }
+    }
+    funcPeresobratMtrix();
     if ($count % 2 === 0) {
       massEl[y][x].state = "black";
+      if (x + 1 < kollSqInLine) {
+        if (massEl[y][x + 1].state === "white") {
+          funcProv(x + 1, y, "white");
+          console.log(stateProv);
+          if (stateProv) {
+            funcPocras(x + 1, y, "white");
+          }
+          stateProv = false;
+          funcPeresobratMtrix();
+        }
+      }
+      if (x - 1 >= 0) {
+        if (massEl[y][x - 1].state === "white") {
+          funcProv(x - 1, y, "white");
+          console.log(stateProv);
+          if (stateProv) {
+            funcPocras(x - 1, y, "white");
+          }
+          stateProv = false;
+          funcPeresobratMtrix();
+        }
+      }
+      if (y + 1 < kollSqInLine) {
+        if (massEl[y + 1][x].state === "white") {
+          funcProv(x, y + 1, "white");
+          console.log(stateProv);
+          if (stateProv) {
+            funcPocras(x, y + 1, "white");
+          }
+          stateProv = false;
+          funcPeresobratMtrix();
+        }
+      }
+      if (y - 1 >= 0) {
+        if (massEl[y - 1][x].state === "white") {
+          funcProv(x, y - 1, "white");
+          console.log(stateProv);
+          if (stateProv) {
+            funcPocras(x, y - 1, "white");
+          }
+          stateProv = false;
+          funcPeresobratMtrix();
+        }
+      }
       gameState = "white";
     } else {
       massEl[y][x].state = "white";
@@ -18,6 +136,7 @@
     }
     count.increment();
   }
+
   for (let i = 0; i < kollSqInLine; i++) {
     massEl.push([]);
     for (let q = 0; q < kollSqInLine; q++) {
@@ -29,8 +148,8 @@
 <h1>Сейчас ходит игрок - {gameState}</h1>
 <div class="go" style="width:{sizeBoard}px; height: {sizeBoard}px">
   {#each massEl as mass}
-    {#each mass as el }
-    <Square {...el} funcPaint={() => funcPaint(el.x, el.y)} />
+    {#each mass as el}
+      <Square {...el} funcPaint={() => funcPaint(el.x, el.y)} />
     {/each}
   {/each}
 </div>
--- src/Square.svelte
+++ src/Square.svelte
@@ -9,8 +9,9 @@
     funcPaint();
   }
 </script>
- 
-<svg on:click|once={funcClick} width={size} height={size} viewBox="0 0 {size} {size}" >
+
+{#if state==="bisque"}
+<svg on:click={funcClick} width={size} height={size} viewBox="0 0 {size} {size}" >
   <g>
    <rect x="0" y="0" width={size} height={size}  fill="#fbefdf"/>
    <g display="none">
@@ -25,6 +26,24 @@
    {/if}
   </g>
  </svg>
+{:else}
+<svg width={size} height={size} viewBox="0 0 {size} {size}" >
+  <g>
+   <rect x="0" y="0" width={size} height={size}  fill="#fbefdf"/>
+   <g display="none">
+    <rect id="svg_4" width="100%" height="100%" x="0" y="0" stroke-width="0" />
+   </g>
+  </g>
+  <g>
+   <line stroke="#999999" x1="0" y1={sizeD} x2={size} y2={sizeD} opacity="0.5"/>
+   <line stroke="#999999" x1={sizeD} y1="0" x2={sizeD} y2={size} opacity="0.5"/>
+   {#if state != "bisque"}
+   <circle cx={sizeD} cy={sizeD} r="15" stroke="black" stroke-width="1" fill={state} />
+   {/if}
+  </g>
+ </svg>
+{/if}
+
 
 <style>
   svg {
